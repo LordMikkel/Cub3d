4678243854495c4a9bb4603c3332f1487dc34d79
LordMikkel <125906789+LordMikkel@users.noreply.github.com>
2026-01-24 23:09:13 +0100

    fix: less than 2 arguments were allowed

--- src/parse/check_args.c
+++ src/parse/check_args.c
@@ -6,7 +6,7 @@
 /*   By: migarrid <migarrid@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/25 18:56:52 by migarrid          #+#    #+#             */
-/*   Updated: 2026/01/03 00:05:01 by migarrid         ###   ########.fr       */
+/*   Updated: 2026/01/24 18:21:32 by migarrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	check_args(int ac, char **av)
 {
-	if (ac > 2)
+	if (ac != 2)
 		exit_error(NULL, ERR_ARGS, EXIT_USE);
 	if (ac == 2)
 	{
